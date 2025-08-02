0990c14efa1c5fb214885ab663bde64f62ff9c55
Jackelin <jackelinvasquezguzman@gmail.com>
2025-08-01 19:42:08 -0600

    match case menu

--- Actividad_13.py
+++ Actividad_13.py
@@ -4,3 +4,22 @@ def menu_principal():
     print("1.Agregar Estudiate.\n2.Agregar curso con nota.\n3.Consultar Estudiante")
     print("4.Calcular promedio de estudiante\n5.Verificar si aprueba\n6.Mostrar todos los estudiantes.\n7.Salir")
 
+menu_principal()
+opcion= input("Ingrese una opcion:")
+
+match opcion:
+    case "1":
+        print("---AGREGAR ESTUDIANTE---")
+    case "2":
+        print("---AGREGAR CURSO CON NOTA---")
+    case "3":
+        print("---CONSULTAR ESTUDIANTE---")
+    case "4":
+        print("---CALCULAR PROMEDIO DE ESTUDIANTE---")
+    case "5":
+        print("---VERIFICAR SI APRUEBA ESTUDIANTE---")
+    case "6":
+        print("---MOSTAR TODOS LOS ESTUDIANTES---")
+    case "7":
+        print("saliendo del sistema...")
+        break
