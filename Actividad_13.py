0e7112668c4458aa47c8c50897b409002763957e
Jackelin <jackelinvasquezguzman@gmail.com>
2025-08-01 19:26:52 -0600

    función menú principal

---
+++ Actividad_13.py
@@ -0,0 +1,6 @@
+#SISTEMA DE GESTIÓN ACADÉMICA
+def menu_principal():
+    print("---MENÚ DE GESTIÓN ACADÉMICA---")
+    print("1.Agregar Estudiate.\n2.Agregar curso con nota.\n3.Consultar Estudiante")
+    print("4.Calcular promedio de estudiante\n5.Verificar si aprueba\n6.Mostrar todos los estudiantes.\n7.Salir")
+
